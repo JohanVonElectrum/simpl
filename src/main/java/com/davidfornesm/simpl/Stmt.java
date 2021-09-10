ec598eb5700e07874c8aff58cde1b8aee6e014f0
David Fornés Martínez <davidfornesm@gmail.com>
2021-09-10 18:58:11 +0200

    reformat source

--- src/main/java/com/davidfornesm/simpl/Stmt.java
+++ src/main/java/com/davidfornesm/simpl/Stmt.java
@@ -3,10 +3,10 @@ package com.davidfornesm.simpl;
 abstract class ProgramStatement {
 
     public abstract State eval(State s); // <i,s> |v| s'
+
     static class EmptyStmt extends ProgramStatement {
 
-        public State eval(State s)
-        {
+        public State eval(State s) {
             return s;
         }
     }
@@ -14,55 +14,66 @@ abstract class ProgramStatement {
 
     static class AssignmentStmt extends ProgramStatement {
 
-        private String x; // a variable is a numeric index
-        private ArithmeticExp a;
+        private final String x; // a variable is a numeric index
+        private final ArithmeticExp a;
 
-        public AssignmentStmt(String name, ArithmeticExp e)
-        { x = name; a = e; }
+        public AssignmentStmt(String name, ArithmeticExp e) {
+            x = name;
+            a = e;
+        }
 
-        public State eval(State s)
-        {  s.setNewBinding(x,a.eval(s));
+        public State eval(State s) {
+            s.setNewBinding(x, a.eval(s));
             return s;
         }
     }
 
     static class SequenceStmt extends ProgramStatement {
 
-        private ProgramStatement c1,c2;
+        private final ProgramStatement c1;
+        private final ProgramStatement c2;
 
-        public SequenceStmt(ProgramStatement s1, ProgramStatement s2)
-        { c1 = s1; c2 = s2; }
+        public SequenceStmt(ProgramStatement s1, ProgramStatement s2) {
+            c1 = s1;
+            c2 = s2;
+        }
 
-        public State eval(State s)
-        {    return c2.eval(c1.eval(s));
+        public State eval(State s) {
+            return c2.eval(c1.eval(s));
         }
     }
 
     static class IfThenElseStmt extends ProgramStatement {
 
-        private BooleanExp b;
-        private ProgramStatement c1,c2;
+        private final BooleanExp b;
+        private final ProgramStatement c1;
+        private final ProgramStatement c2;
 
-        public IfThenElseStmt(BooleanExp e, ProgramStatement s1, ProgramStatement s2)
-        { b = e; c1 = s1; c2 = s2; }
+        public IfThenElseStmt(BooleanExp e, ProgramStatement s1, ProgramStatement s2) {
+            b = e;
+            c1 = s1;
+            c2 = s2;
+        }
 
-        public State eval(State s)
-        {   if (b.eval(s)) return c1.eval(s);
-        else return c2.eval(s);
+        public State eval(State s) {
+            if (b.eval(s)) return c1.eval(s);
+            else return c2.eval(s);
         }
     }
 
     static class WhileStmt extends ProgramStatement {
 
-        private BooleanExp b;
-        private ProgramStatement c;
+        private final BooleanExp b;
+        private final ProgramStatement c;
 
-        public WhileStmt(BooleanExp e, ProgramStatement s)
-        { b = e; c = s; }
+        public WhileStmt(BooleanExp e, ProgramStatement s) {
+            b = e;
+            c = s;
+        }
 
-        public State eval(State s)
-        {   if (b.eval(s)) return this.eval(c.eval(s));
-        else return s;
+        public State eval(State s) {
+            if (b.eval(s)) return this.eval(c.eval(s));
+            else return s;
         }
     }
 }
