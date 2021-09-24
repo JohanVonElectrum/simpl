afa58f9a5d4e6601c6bc8b93e13ac49197d29885
David Fornés Martínez <davidfornesm@gmail.com>
2021-09-24 21:53:38 +0200

    very raw implementation

--- src/main/java/com/davidfornesm/simpl/App.java
+++ src/main/java/com/davidfornesm/simpl/App.java
@@ -1,9 +1,13 @@
 package com.davidfornesm.simpl;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.*;
+import java.net.InetSocketAddress;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
@@ -24,6 +28,12 @@ public class App
         if (args.length > 2) {
             System.out.println("Usage: simpl debug || [source] (compile) || [source] (debug)");
             System.exit(64);
+        } else if (args.length == 1 && args[0].equals("server")) {
+            HttpServer httpServer = HttpServer.create();
+            httpServer.bind(new InetSocketAddress("localhost", 8003), 0);
+            httpServer.createContext("/", new PromptHandler());
+            httpServer.createContext("/static", new StaticHandler());
+            httpServer.start();
         } else if ((args.length == 1 && !args[0].equals("debug")) || args.length == 2) {
             if (args.length == 2 && Objects.equals(args[1], "debug")) debug = true;
             if (args.length == 2 && Objects.equals(args[1], "compile")) compile = true;
@@ -31,6 +41,49 @@ public class App
         } else {
             if (args.length == 1) debug = true;
             runPrompt();
+        }
+    }
+
+    static class PromptHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            if (httpExchange.getRequestMethod().equals("GET")) {
+                OutputStream outputStream = httpExchange.getResponseBody();
+                String response = Files.readString(Paths.get("server.html"));
+                response = response.replaceFirst("@submission", "");
+                response = response.replaceFirst("@evaluation", "Results will appear here...");
+                httpExchange.sendResponseHeaders(200, response.length());
+                outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+                outputStream.close();
+            } else if (httpExchange.getRequestMethod().equals("POST")) {
+                OutputStream outputStream = httpExchange.getResponseBody();
+                InputStream inputStream = httpExchange.getRequestBody();
+                String submission = (new String(inputStream.readAllBytes(), Charset.defaultCharset())).substring(11);
+                String evaluation = "result := " + run(submission, null).toString();
+                String response = Files.readString(Paths.get("server.html"));
+                response = response.replaceFirst("@submission", submission);
+                response = response.replaceFirst("@evaluation", evaluation);
+                httpExchange.sendResponseHeaders(200, response.length());
+                outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+                outputStream.flush();
+                outputStream.close();
+                inputStream.close();
+            }
+        }
+    }
+
+    static class StaticHandler implements HttpHandler {
+
+        @Override
+        public void handle(HttpExchange httpExchange) throws IOException {
+            OutputStream outputStream = httpExchange.getResponseBody();
+            String response = Files.readString(Paths.get("bootstrap.min.css"), Charset.defaultCharset());
+            httpExchange.sendResponseHeaders(200, 0);
+            outputStream.write(response.getBytes(StandardCharsets.UTF_8));
+            outputStream.flush();
+            outputStream.close();
         }
     }
 
@@ -59,7 +112,7 @@ public class App
         if (hadError) System.exit(65);
     }
 
-    private static void run(String source, State state) {
+    private static Integer run(String source, State state) {
         if (state == null) state = new State();
         Scanner scanner = new Scanner(source);
         List<Token> tokens = scanner.scanTokens();
@@ -71,6 +124,8 @@ public class App
         if (debug) emitDebug(tokens, program);
         if (!compile) System.out.println("result := " + result);
         if (compile) compileSourceCode(program);
+
+        return result;
     }
 
     private static void emitDebug(List<Token> tokens, ProgramStatement program) {
